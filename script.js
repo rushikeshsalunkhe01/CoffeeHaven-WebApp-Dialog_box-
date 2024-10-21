9265db3d539224181046a87dc38fe36b0f61b05e
rushikeshsalunkhe01 <rushisalunkhe111053@gmail.com>
2024-10-21 13:32:22 +0530

    add radme file with imges

---
+++ script.js
@@ -0,0 +1,9 @@
+function showModal() {
+    const modal = document.getElementById('modalOverlay');
+    modal.style.display = 'flex';
+}
+
+function hideModal() {
+    const modal = document.getElementById("modalOverlay");
+    modal.style.display = "none";
+}
